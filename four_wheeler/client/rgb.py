a25e6df7b0f372c1a4b4276d62efa7867ae2d389
nocturnalplay <106279384+nocturnalplay@users.noreply.github.com>
2023-03-17 02:59:12 +0530

    Add files via upload

--- four_wheeler/client/rgb.py
+++ four_wheeler/client/rgb.py
@@ -8,7 +8,7 @@ import json
 import threading
 import math
 
-channel = [16,20,21]
+channel = [16, 20, 21]
 
 # for board setting
 GPIO.setmode(GPIO.BCM)
@@ -18,12 +18,13 @@ for i in channel:
     GPIO.setup(i, GPIO.OUT)
 
 
-#rgb in pwm mode in a list
-rgbcolor= [GPIO.PWM(c,100) for c in channel]
+# rgb in pwm mode in a list
+rgbcolor = [GPIO.PWM(c, 100) for c in channel]
 
 # start the birghtness with 100
 for i in range(len(rgbcolor)):
     rgbcolor[i].start(100)
+
 
 def findPercents(inp, mi, ma, v):
     va = (inp - mi) * 100 / (ma - mi)
@@ -40,97 +41,38 @@ def findPercents(inp, mi, ma, v):
 def AccelerationOperation(rightHand):
     try:
         if len(rightHand) > 0:
-            x0, x1 = rightHand[0][0], rightHand[12][0]
-            y0, y1 = rightHand[0][1], rightHand[12][1]
-            x3, x4 = rightHand[3][0], rightHand[4][0]
-            acc = findPercents(
-                math.hypot(x0-x1, y0-y1), 50, 140, 0)
-            # accleration speed start
-            motorDriver1_1.start(acc)
-            motorDriver1_2.start(acc)
-            motorDriver2_1.start(acc)
-            motorDriver2_2.start(acc)
-            # neutral Acceleration
-            if acc > 0:
-                angle = abs(math.atan2(y1 - y0, x1 - x0) * 180 / math.pi)
-                print(angle)
-                if angle < 60:
-                    GPIO.output(13, 0)
-                    GPIO.output(19, 0)
-                    GPIO.output(5, 1)
-                    GPIO.output(6, 0)
-                    GPIO.output(27, 1)
-                    GPIO.output(22, 0)
-                    GPIO.output(10, 0)
-                    GPIO.output(9, 0)
-                elif angle > 120:
-                    GPIO.output(13, 1)
-                    GPIO.output(19, 0)
-                    GPIO.output(5, 0)
-                    GPIO.output(6, 0)
-                    GPIO.output(27, 0)
-                    GPIO.output(22, 0)
-                    GPIO.output(10, 1)
-                    GPIO.output(9, 0)
-                else:
-                    if x3 > x4:
-                        print("direction back")
-                        GPIO.output(13, 0)
-                        GPIO.output(19, 1)
-                        GPIO.output(5, 0)
-                        GPIO.output(6, 1)
-                        GPIO.output(27, 0)
-                        GPIO.output(22, 1)
-                        GPIO.output(10, 0)
-                        GPIO.output(9, 1)
-                    else:  # forward Acceleration
-                        print("direction front")
-                        GPIO.output(13, 1)
-                        GPIO.output(19, 0)
-                        GPIO.output(5, 1)
-                        GPIO.output(6, 0)
-                        GPIO.output(27, 1)
-                        GPIO.output(22, 0)
-                        GPIO.output(10, 1)
-                        GPIO.output(9, 0)
+            # rgb x and y axis point
+            x0, y0 = rightHand[0][0], rightHand[0][1]
+            rx, ry = rightHand[4][0], rightHand[4][1]
+            gx, gy = rightHand[8][0], rightHand[8][1]
+            bx, by = rightHand[12][0], rightHand[12][1]
 
-            else:
-                GPIO.output(13, 0)
-                GPIO.output(19, 0)
-                GPIO.output(5, 0)
-                GPIO.output(6, 0)
-                GPIO.output(27, 0)
-                GPIO.output(22, 0)
-                GPIO.output(10, 0)
-                GPIO.output(9, 0)
-            print("Acceleration:", acc)
-        else:
-            motorDriver1_1.start(0)
-            motorDriver1_2.start(0)
-            motorDriver2_1.start(0)
-            motorDriver2_2.start(0)
-            GPIO.output(13, 0)
-            GPIO.output(19, 0)
-            GPIO.output(5, 0)
-            GPIO.output(6, 0)
-            GPIO.output(27, 0)
-            GPIO.output(22, 0)
-            GPIO.output(10, 0)
-            GPIO.output(9, 0)
+            # canculate the length between the bottom to the each point
+            Rlen = [findPercents(math.hypot(rx - x0, ry - y0), 155, 185, 0),
+                    findPercents(math.hypot(rx - x0, ry - y0), 155, 185, 100)]
+            Glen = [findPercents(math.hypot(gx - x0, gy - y0), 140, 240, 0),
+                    findPercents(math.hypot(gx - x0, gy - y0), 140, 240, 100)]
+            Blen = [findPercents(math.hypot(bx - x0, by - y0), 120, 260, 0),
+                    findPercents(math.hypot(bx - x0, by - y0), 120, 260, 100)]
+
+            rgb = [Rlen[1], Glen[1], Blen[1]]
+            print(rgb)
+
+            for i in range(3):
+                rgbcolor[i].ChangeDutyCycle(rgb[i])
     except KeyboardInterrupt:
-        print("Force exit operation")
-        motorDriver1_1.start(0)
-        motorDriver1_2.start(0)
-        motorDriver2_1.start(0)
-        motorDriver2_2.start(0)
-        GPIO.output(13, 0)
-        GPIO.output(19, 0)
-        GPIO.output(5, 0)
-        GPIO.output(6, 0)
-        GPIO.output(27, 0)
-        GPIO.output(22, 0)
-        GPIO.output(10, 0)
-        GPIO.output(9, 0)
+        GPIO.cleanup()
+        # end the PWM
+        for i in range(3):
+            rgbcolor[i].stop()
+        print("\nExit....")
+        # close the socket connection
+        sys.exit()
+
+    except ValueError as e:
+        print(f"[ERROR]:{e}")
+        print("try again...")
+        GPIO.cleanup()
         sys.exit()
 
 
@@ -146,18 +88,7 @@ def AccessingTheGPIO(handData):
         # after defineing the thread model we need to start the thread
         rightThread.start()
     else:
-        motorDriver1_1.start(0)
-        motorDriver1_2.start(0)
-        motorDriver2_1.start(0)
-        motorDriver2_2.start(0)
-        GPIO.output(13, 0)
-        GPIO.output(19, 0)
-        GPIO.output(5, 0)
-        GPIO.output(6, 0)
-        GPIO.output(27, 0)
-        GPIO.output(22, 0)
-        GPIO.output(10, 0)
-        GPIO.output(9, 0)
+        pass
 # RabbitMQ receiveing data
 
 
@@ -171,15 +102,25 @@ def callback(ch, method, properties, body):
 
 
 if __name__ == "__main__":
-    cred = pika.PlainCredentials('anish', 'dotmail123')
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='172.19.0.2', port=5672, virtual_host='/', credentials=cred))
-    channel = connection.channel()
+    try:
+        cred = pika.PlainCredentials('anish', 'dotmail123')
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='172.19.0.2', port=5672, virtual_host='/', credentials=cred))
+        channel = connection.channel()
 
-    channel.queue_declare(queue='hand_gesture_data')
+        channel.queue_declare(queue='hand_gesture_data')
 
-    channel.basic_consume(queue='hand_gesture_data',
-                          on_message_callback=callback, auto_ack=True)
+        channel.basic_consume(queue='hand_gesture_data',
+                              on_message_callback=callback, auto_ack=True)
 
-    print('Waiting for hand gesture data. To exit press CTRL+C')
-    channel.start_consuming()
+        print('Waiting for hand gesture data. To exit press CTRL+C')
+        channel.start_consuming()
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        # end the PWM
+        for i in range(3):
+            rgbcolor[i].stop()
+        print("\nExit....")
+        # close the socket connection
+        sys.exit()
